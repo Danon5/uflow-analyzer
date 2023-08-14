e08503f1694d923d0926cd371f29db56313fea93
Danon5 <danonfive@gmail.com>
2023-08-14 01:55:19 -0700

    added separate analyzer ids

--- UFlowAnalyzer/AnalyzerIds.cs
+++ UFlowAnalyzer/AnalyzerIds.cs
@@ -6,10 +6,11 @@
 
         public static class Warnings {
             public const string LOGIC_PRESERVE = "UFW0001";
+            public const string SERIALIZER_PRESERVE = "UFW0002";
         }
 
         public static class Errors {
-            
+            public const string RPC_METHOD_PARAMETERS = "UFE0001";
         }
     }
 }
